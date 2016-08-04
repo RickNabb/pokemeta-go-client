71a40b09cbfbc9fb5ea7cc79fdbc614179b0f91c
RickNabb <nrabb@outlook.com>
2016-08-04 11:42:28 -0400

    Added react components and webpack to package. In the process of getting webpack and react to work

--- app/client.js
+++ app/client.js
@@ -1,3 +1,26 @@
-define(function (require) {
-  console.log('test');
-});
+/**
+* client.js
+* (C) Yes And Games 2016
+* Nick Rabb (nrabb@outlook.com)
+* The main client for the pokemeta-go client.
+*/
+
+var React = require('react');
+var Login = require("./app/components/login");
+
+var STATE_LOGIN = 0;
+var STATE_MAIN = 1;
+
+function changePage(key) {
+  'use strict';
+  switch (key) {
+    case (STATE_LOGIN):
+      React.render(
+        <Login />,
+        document.getElementById('content')
+        );
+      break;
+    case (STATE_MAIN):
+      break;
+  }
+}
